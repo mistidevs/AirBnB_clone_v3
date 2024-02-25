33a4a03e672814a13a849f73123dd81e730f7677
mistidevs <mistidevs@gmail.com>
2024-02-25 07:47:25 -0800

    code(index.py) : stats api call to get statistics of all classes ; frustration(index.py) : jsonify format not the expected

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,8 +3,20 @@
 Creating routes
 """
 from flask import jsonify
+from models import storage
 from api.v1.views import app_views
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+from models.base_model import BaseModel
 
+
+classes = {"amenities": Amenity, "cities": City,
+           "places": Place, "reviews": Review,
+           "states": State, "users": User}
 
 @app_views.route("/status", methods=["GET"])
 def status_ok():
@@ -12,3 +24,12 @@ def status_ok():
     return (jsonify({
         "status": "OK"
     }))
+
+@app_views.route("/stats", methods=["GET"])
+def stats():
+    """ Yields statistics for the database"""
+    stat_dict = {}
+    for cls in classes:
+        stat_dict[cls] = storage.count(classes[cls])
+
+    return (jsonify(stat_dict))
