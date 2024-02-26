6a55d7405cc1e13a0e58f80380c38039b958c091
mistidevs <mistidevs@gmail.com>
2024-02-26 20:15:40 +0300

    code(basemodel.py, user.py) : Hashing user passwords

--- models/user.py
+++ models/user.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ holds class User"""
 import models
+import hashlib
 from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
@@ -27,3 +28,4 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+        self.password = hashlib.md5(self.password.encode()).hexdigest()
