e6e33fcfe5b8cebcfbc4ea8bff5fc80f62118ae5
mistidevs <mistidevs@gmail.com>
2024-02-26 21:03:55 +0300

    code(places_amenities.py) : Fixing typo in file name

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -15,4 +15,4 @@ if app_views is not None:
     from api.v1.views.users import *
     from api.v1.views.places import *
     from api.v1.views.reviews import *
-    from api.v1.views.place_amenities import *
+    from api.v1.views.places_amenities import *
